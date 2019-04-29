dd904796c129e067e184d2591a784b2842334e44
Pramukta Kumar <pramukta.kumar@gmail.com>
2019-04-29 12:51:29 -0500

    transfer and update of basic setup.py for the repository

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = "mftrees",
+    version = "0.0.1",
+    author = "Pramukta Kumar",
+    author_email = "pramukta@asu.edu",
+    description = ("Rainforest carbon estimation from satellite imagery using Fourier power spectra, Manifold embeddings, and xgboost"),
+    url = "https://github.com/pramukta/mftrees",
+    packages=['mftrees'],
+    entry_points={
+        "console_scripts": [
+            "ftfeat = mftrees.features:oneshot"
+        ]
+    }
+)
