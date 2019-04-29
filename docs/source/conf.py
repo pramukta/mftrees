37854f6ddf78034ebbe90bd949adda548c7d9ec7
Pramukta Kumar <pramukta.kumar@gmail.com>
2019-04-29 13:05:59 -0500

    enabling sphinx_click extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,8 @@ release = '0.0.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx_click.ext'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
