b3ecd4976f36f298043622e79e83dc050b85823f
Pramukta Kumar <pramukta.kumar@gmail.com>
2019-05-06 10:36:11 -0500

    temporarily remove the classify cli routine (will likely need modification)

--- mftrees/models.py
+++ mftrees/models.py
@@ -19,68 +19,6 @@ from mftrees.features import fmap, full_feature_vector
 @click.option("--augment-file", "-a", default="srtm.tif", type=click.Path(exists=True),
               help="Prepressed augmentation data file as a GeoTIFF")
 @click.option("-o", "--out", default="classes.tif", type=click.Path(), help="classification output geotiff")
-@click.argument("model_file", type=click.Path(exists=True))
-def classify(model_file, mosaic_file, augment_file, out, chunk_size=50000):
-    # NOTE: A bunch of extra data is needed in the serialized model file
-    model_data = joblib.load(model_file)
-    model = model_data["model"]
-    pixel_size = model_data["pixel_size"]
-    n_bands = model_data["n_bands"]
-    model_res = tuple(model_data["res"])
-    bin_map = model_data["bin_map"]
-
-    print(model_res)
-
-    with rasterio.open(augment_file) as dataset:
-        profile = dataset.profile
-        tfm = dataset.transform
-        target = dataset.read(1)
-        output = np.zeros_like(target)
-        coords = np.vstack(reversed(np.where(target != dataset.nodata)))
-        print(f"{coords.shape[1]} valid cells")
-
-        # TODO: assert that the augment file's projection matches model's expected projection
-        if(dataset.res != model_res):
-            warnings.warn(f"Data resolution {dataset.res} differs from model's expected resolution {model_res}")
-
-        # set up windows
-        left, top = tfm*coords
-        right, bottom = tfm*(coords + np.ones((2, 1)))
-
-        augment_data = np.vstack([dataset.read(b+1).ravel() for b in range(dataset.count)]).T
-
-        with rasterio.open(mosaic_file) as mosaic:
-            out_shape = (mosaic.count, *[int(e/pixel_size) for e in dataset.res])
-            assert mosaic.count == n_bands, f"{mosaic.count} bands found.  {n_bands} expected"
-            with WarpedVRT(mosaic, crs=profile["crs"], resampling=Resampling.bilinear) as vrt:
-                cb = list(range(0, left.size, chunk_size)) + [left.size]
-                for i in trange(len(cb) - 1, ncols=100):
-                    batch = []
-                    for idx, wnd in tqdm(enumerate(zip(left[cb[i]:cb[i+1]], bottom[cb[i]:cb[i+1]],
-                                                       right[cb[i]:cb[i+1]], top[cb[i]:cb[i+1]])),
-                                         total=cb[i+1]-cb[i], ncols=100):
-                        dst_wnd = vrt.window(*wnd)
-                        data = vrt.read(window=dst_wnd, out_shape=out_shape)
-                        batch.append(full_feature_vector(data, bin_map).ravel())
-                    batch = np.hstack([np.vstack(batch), augment_data[cb[i]:cb[i+1]]])
-                    # TODO: this is broken with new structure
-                    output[coords[1, cb[i]:cb[i+1]], coords[0, cb[i]:cb[i+1]]] = model.named_steps["reg"].clusterer.predict(model.named_steps["embed"].transform(batch)[:,:-1]) + 1
-
-    print("\n\n")
-    out_profile = profile.copy()
-    out_profile["dtype"] = np.uint8
-    out_profile["nodata"] = 0
-
-    with rasterio.open(out, "w", **out_profile) as outfile:
-        outfile.write(output.astype(np.uint8), 1)
-
-
-@click.command(help="MODELS_FILE: joblib-serialized carbon estimation model")
-@click.option("--mosaic-file", default="mosaic.tif", type=click.Path(exists=True),
-              help="Preprocessed image mosaic file as a GeoTIFF")
-@click.option("--augment-file", "-a", default="srtm.tif", type=click.Path(exists=True),
-              help="Prepressed augmentation data file as a GeoTIFF")
-@click.option("-o", "--out", default="classes.tif", type=click.Path(), help="classification output geotiff")
 @click.option("--blm/--no-blm", help="Base Layer Match mosaic to reference", default=False)
 @click.option("--reference", default="reference.vrt", help="Reference mosaic used for baselayer matching")
 @click.argument("model_file", type=click.Path(exists=True))
