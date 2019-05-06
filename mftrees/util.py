b9f5c3cb458abf2d612c420c593bdb0b7b644746
Pramukta Kumar <pramukta.kumar@gmail.com>
2019-05-05 19:33:52 -0500

    adding in a histogram matching helper function

--- mftrees/util.py
+++ mftrees/util.py
@@ -1,4 +1,6 @@
+from functools import partial
 import sys
+
 import numpy as np
 
 
@@ -35,3 +37,34 @@ def r2(y_pred, y, w=1.0):
         Weights for each sample (default is 1.0)
     """
     return  1 - np.sum(w*(y - y_pred)**2)/np.sum(w*(y - y.mean())**2)
+
+
+def create_histmatcher(img, ref):
+    """
+    Create a function that will histogram match pieces of a source image with a target image, incrementally
+
+    Parameters
+    ----------
+    img : ndarray
+        NumPy array containing the image data to transform
+    ref : ndarray
+        Numpy array containing the reference image data for the transformation
+
+    Returns
+    -------
+    function
+        A function that takes a 2-D NumPy array to be transformed
+
+    """
+    t_vals, t_counts = np.unique(ref.ravel(), return_counts=True)
+    t_cdf = np.cumsum(t_counts).astype(np.double) / ref.size
+
+    s_vals, s_counts = np.unique(img.ravel(), return_counts=True)
+    s_cdf = np.cumsum(s_counts).astype(np.double) / img.size
+
+    def _matcher(chunk):
+        blmfn = partial(np.interp, xp=t_cdf, fp=t_vals)
+        cdffn = partial(np.interp, xp=s_vals, fp=s_cdf)
+        return blmfn(cdffn(chunk))
+
+    return _matcher
